6f15735cdc9773aab718bb6c849a09734b77a2d7
monichoo <monichoo@gmail.com>
2020-08-31 17:46:33 +0800

    Updated About Page to test pulling git

--- src/Views/About.js
+++ src/Views/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About(){
     return(
         <div>
-            <h1 className="font-bold text-2xl">About the Watchers</h1>
+            <h1 className="font-bold text-2xl"> LALALALALALAL PULL THIS  About the Watchers</h1>
         </div>
     );
 }
