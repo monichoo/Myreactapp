185893b7b1722f8cce9fd03d0b472acad4c0af71
monichoo <monica@asiadiveacademy.com>
2020-08-31 19:08:12 +0800

    Link Photos to indivdual Photo Pages

--- src/Components/PhotoCard.js
+++ src/Components/PhotoCard.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function PhotoCard(props){
     return(
         <div className="border mb-4 rounded overflow-hidden">
-            <div
-                style={{
-                    'backgroundImage': `url('${props.photo.thumbnailUrl}')`
-                }}
-                className="w-full h-64 bg-blue bg-cover"
-            >
+            <Link to={`/photos/${props.photo.id}`}>
+                <div
+                    style={{
+                        'backgroundImage': `url('${props.photo.thumbnailUrl}')`
+                    }}
+                    className="w-full h-64 bg-blue bg-cover"
+                >
 
+                </div>
+            </Link>
+            <div className="p-3">
+                <h1 className="font-bold text-xl mb-3">
+                    <Link to={`/photos/${props.photo.id}`}>
+                        {props.photo.title}
+                    </Link>
+                    {props.photo.id}
+                </h1>
             </div>
-            {props.photo.title}
-            {props.photo.id}
         </div>
     )
 }
